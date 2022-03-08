3b7fd291070a3a714f2d28f974d747545ca90604
Tom Hurtt <tom.hurtt@psikick.com>
2022-03-08 16:53:36 -0500

    Fixed an issue with the test execution id response and updated tests to cover both cases

--- src/pytest_xray/xray_publisher.py
+++ src/pytest_xray/xray_publisher.py
@@ -12,7 +12,6 @@ _logger = logging.getLogger(__name__)
 
 
 class BearerAuth(AuthBase):
-
     def __init__(self, base_url: str, client_id: str, client_secret: str) -> None:
         if base_url.endswith('/'):
             base_url = base_url[:-1]
@@ -25,21 +24,11 @@ class BearerAuth(AuthBase):
         return f'{self.base_url}{AUTHENTICATE_ENDPOINT}'
 
     def __call__(self, r: requests.PreparedRequest) -> requests.PreparedRequest:
-        headers = {
-            'Content-type': 'application/json',
-            'Accept': 'text/plain'
-        }
-        auth_data = {
-            'client_id': self.client_id,
-            'client_secret': self.client_secret
-        }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        auth_data = {'client_id': self.client_id, 'client_secret': self.client_secret}
 
         try:
-            response = requests.post(
-                self.endpoint_url,
-                data=json.dumps(auth_data),
-                headers=headers
-            )
+            response = requests.post(self.endpoint_url, data=json.dumps(auth_data), headers=headers)
         except requests.exceptions.ConnectionError as exc:
             err_message = f'ConnectionError: cannot authenticate with {self.endpoint_url}'
             _logger.exception(err_message)
@@ -51,7 +40,6 @@ class BearerAuth(AuthBase):
 
 
 class ApiKeyAuth(AuthBase):
-
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
@@ -61,13 +49,8 @@ class ApiKeyAuth(AuthBase):
 
 
 class XrayPublisher:
-
     def __init__(
-        self,
-        base_url: str,
-        endpoint: str,
-        auth: Union[AuthBase, tuple],
-        verify: Union[bool, str] = True
+        self, base_url: str, endpoint: str, auth: Union[AuthBase, tuple], verify: Union[bool, str] = True
     ) -> None:
         if base_url.endswith('/'):
             base_url = base_url[:-1]
@@ -81,14 +64,10 @@ class XrayPublisher:
         return self.base_url + self.endpoint
 
     def _send_data(self, url: str, auth: Union[AuthBase, tuple], data: dict) -> dict:
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
         try:
             response = requests.request(
-                method='POST', url=url, headers=headers, json=data,
-                auth=auth, verify=self.verify
+                method='POST', url=url, headers=headers, json=data, auth=auth, verify=self.verify
             )
         except requests.exceptions.ConnectionError as exc:
             err_message = f'ConnectionError: cannot connect to JIRA service at {url}'
@@ -98,8 +77,10 @@ class XrayPublisher:
             try:
                 response.raise_for_status()
             except requests.exceptions.HTTPError as exc:
-                err_message = (f'HTTPError: Could not post to JIRA service at {url}. '
-                               f'Response status code: {response.status_code}')
+                err_message = (
+                    f'HTTPError: Could not post to JIRA service at {url}. '
+                    f'Response status code: {response.status_code}'
+                )
                 _logger.exception(err_message)
                 if 'error' in response.json():
                     _logger.error('Error message from server: %s', response.json()['error'])
@@ -114,5 +95,5 @@ class XrayPublisher:
         :return: test execution issue id
         """
         response_data = self._send_data(self.endpoint_url, self.auth, data)
-        key = response_data['key']
+        key = response_data['testExecIssue']['key'] if 'testExecIssue' in response_data else response_data['key']
         return key
