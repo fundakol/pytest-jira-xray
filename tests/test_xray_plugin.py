cd80cb82b7017bd431a9fe3c4de7d3d46a160c7d
Ronan Viel <ronan.viel@orange.com>
2023-07-10 14:38:33 +0200

    Fix coverage target (codecov) breach

--- tests/test_xray_plugin.py
+++ tests/test_xray_plugin.py
@@ -112,9 +112,10 @@ def test_if_user_can_modify_results_with_hooks(xray_tests):
     assert xray_result['info']['user'] == 'Test User'
 
 
-def test_if_user_can_attache_evidences(xray_tests):
+def test_if_user_can_attach_evidences(xray_tests):
     expected_tests = [
-        {'evidences': [
+        {'comment': '{noformat:borderWidth=0px|bgColor=transparent}Test{noformat}',
+         'evidences': [
              {
                  'contentType': 'plain/text',
                  'data': 'ZXZpZGVuY2U=',
@@ -168,6 +169,7 @@ def test_if_user_can_attache_evidences(xray_tests):
         def pytest_runtest_makereport(item, call):
             outcome = yield
             report = outcome.get_result()
+            report.longrepr = "Test"
             evidences = getattr(report, "evidences", [])
             if report.when == "call":
                 evidences.append(
